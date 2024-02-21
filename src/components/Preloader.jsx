a9faeb92a93f191e1203c82e2cd01276be2e07ef
alkachhirang <alkachhirang3@gmail.com>
2024-02-21 16:36:16 +0530

    preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -3,7 +3,7 @@ import { Loader } from './Iconimg';
 
 const Preloader = () => {
     return (
-        <div className='relative'>
+        <div className='relative overflow-hidden'>
             <div className=" absolute left-[-5%] top-[-4%] blur-[124px] opacity-[70%]  w-[274px] h-[274px] rounded-[50%] bg-[#0066FF] animate-pulse md:block hidden"></div>
             <div className=" absolute right-[-5%] top-[25%] blur-[124px] opacity-[70%]  w-[274px] h-[274px] rounded-[50%] bg-[#0066FF] animate-pulse md:block hidden"></div>
             <div className='min-h-screen flex flex-col gap-[12px] justify-center items-center bg-[#000]'>

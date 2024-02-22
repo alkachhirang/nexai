44b4274449a4e61bb257943d0818c3d0689c7ca5
alkachhirang <alkachhirang3@gmail.com>
2024-02-22 14:26:48 +0530

    images

--- src/components/HomeAssistant.jsx
+++ src/components/HomeAssistant.jsx
@@ -8,8 +8,8 @@ const HomeAssistant = () => {
             <div className="absolute left-0 bottom-[-38px] right-0 w-full h-[121px] bg-gradient-to-b from-[#08090e00] to-[#08090E] z-[1] "> </div>
             <div className=" absolute left-0 top-[46%] blur-[124px] opacity-[70%]  w-[274px] h-[168px] rounded-[50%] bg-[#0066FF] animate-pulse md:block hidden"></div>
             <div className="container lg:max-w-[1124px] lg:px-3 px-5 mx-auto">
-                <div className='flex xl:flex-row flex-col-reverse flex-wrap items-center xl:justify-between'>
-                    <div className='xl:w-[55%] w-full pt-[30px] xl:pt-0' data-aos="fade-right">
+                <div className='flex flex-row flex-wrap items-center xl:justify-between'>
+                    <div className='xl:w-[55%] w-full' data-aos="fade-right">
                         <h2 className='ff_play font-normal text-[32px] sm:text-[48px] leading-[42px] sm:leading-[60.48px] text-[#FCFCFC] mb-[8px] sm:mb-[16px] xl:text-start text-center'>Ai-Powered Home Assistant</h2>
                         <p className='font-normal text-[14px] sm:text-[16px] leading-[25.6px] text-[#fff] xl:text-start text-center opacity-70 '>Nexai is an advanced AI-powered home assistant that brings the future to your fingertips. With its ability to engage in natural, fluent conversations in over 15 languages,</p>
                         <p className='font-normal text-[14px] sm:text-[16px] leading-[25.6px] text-[#fff] xl:text-start text-center opacity-70'>Nexai offers an incredibly futuristic experience. It can take the initiative to ask about your day, schedule meetings, provide timely reminders</p>
@@ -20,7 +20,7 @@ const HomeAssistant = () => {
                             </button>
                         </div>
                     </div>
-                    <div className='xl:w-[40%] flex w-full' data-aos="fade-left">
+                    <div className='xl:w-[40%] flex w-full pt-[40px] xl:pt-0' data-aos="fade-left">
                         <div className='relative w-full flex justify-center items-center xl:justify-end'>
                             <img src={Assistantgif} alt="Assistantgif" className='absolute mix-blend-color-dodge max-w-[210px] sm:max-w-[301px] w-full left-[46%] bottom-[2%] ' />
                             <img src={Assistantimg} alt="Assistantimg" className='max-w-[310px] sm:max-w-[443px] w-full relative z-[2] ' />
